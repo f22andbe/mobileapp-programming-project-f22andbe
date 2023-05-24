1e4940bf38a505458067770c110913fe80dd7b23
f22andbe <f22andbe@student.his.se>
2023-05-24 19:07:54 +0200

    Add About activity

---
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        imageView = findViewById(R.id.logoimage);
+        textView = findViewById(R.id.textView);
+
+        // Set image for the about View
+        imageView.setImageResource(R.drawable.logo);
+        // Set the text for the TextView
+        textView.setText(R.string.about_string);
+    }
+}
